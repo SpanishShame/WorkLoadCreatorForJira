ff8991bacdfabd9420349e619e83a6a1652b48f3
SpanishShame <116386294+SpanishShame@users.noreply.github.com>
2022-10-21 22:12:28 +0300

    Add files via upload

---
+++ Entity/Report.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiraWorkloadReportCreator.Entity
+{
+    public class Report
+    {
+        public string Author { get; set; }
+        public double Sum { get; set; }
+        public WorkTask[] Tasks { get; set; }
+    }
+
+    public class WorkTask
+    {
+        public double WorkTime { get; set; }
+        public string TaskId { get; set; }
+        public DateTime Date {get; set; }
+        public string Comment { get; set; }
+    }
+}
