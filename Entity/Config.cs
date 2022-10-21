8da1d62045271fccd1bbf2131c8029f713182b3e
SpanishShame <116386294+SpanishShame@users.noreply.github.com>
2022-10-21 22:12:18 +0300

    Create Config.cs

---
+++ Entity/Config.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JiraWorkloadReportCreator.Entity
+{
+    public class JiraConfig
+    {
+        public string JiraUrl { get; set; }
+        public string JiraTaskUrl { get; set; }
+        public string JiraLogin { get; set; }
+        public string JiraToken { get; set; }
+        public string ReportPath { get; set; }
+    }
+}
